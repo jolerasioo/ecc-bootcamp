930b04515db1fc049496031bf15148b77b9700aa
jolerasioo <90843031+jolerasioo@users.noreply.github.com>
2024-05-15 11:40:06 +0000

    added db

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, g
 import os
+
+from database import db, Todo
 
 
 app = Flask(__name__)
@@ -7,40 +9,48 @@ app = Flask(__name__)
 todo_list = []
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-todo_file = os.path.join(basedir, "todo_list.txt")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "todos.db")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+# database initialization
+db.init_app(app)
+with app.app_context():
+    db.create_all()
 
-try:
-    with open("todo_list.txt", "r") as file:
-        for line in file:
-            todo_list.append(line.strip())
-except FileNotFoundError:
-    print("No saved items found")
+# file to store the to-do list
+@app.before_request
+def load_data_to_g():
+    todos = Todo.query.all()
+    g.todos = todos
+    g.todo = None
 
+# index or home page
 @app.route("/")
 def index():
-    return render_template("index.html", todo_list=todo_list)
+    return render_template("index.html")
 
+# add a to-do item
 @app.route("/add", methods=["POST"])
 def add_todo():
-    todo = request.form["todo"]
-    todo_list.append(todo)
-    save_todo_list()
+    # get the data from the form
+    todo = Todo(
+        name=request.form["todo"],
+    )
+
+    # add the new ToDo to the list
+    db.session.add(todo)
+    db.session.commit()
+    
+    # add the new ToDo to the list
     return redirect(url_for("index"))
 
-@app.route("/remove", methods=["POST"])
-def remove_todo():
-    item_number = int(request.form["item_number"])
-    if 0 < item_number <= len(todo_list):
-        todo_list.pop(item_number - 1)
-        save_todo_list()
+# remove a to-do item
+@app.route("/remove/<int:id>", methods=["GET", "POST"])
+def remove_todo(id):
+    db.session.delete(Todo.query.filter_by(id=id).first())
+    db.session.commit()
     return redirect(url_for("index"))
 
-
-def save_todo_list():
-    with open(todo_file, "w") as file:
-        for todo in todo_list:
-            file.write(todo + "\n")
 
 
 
