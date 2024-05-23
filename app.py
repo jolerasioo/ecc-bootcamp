2e690a884aaacd07edbeed74ea4a2c0986733f23
jolerasioo <90843031+jolerasioo@users.noreply.github.com>
2024-05-23 10:30:08 +0000

    update

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, g
 import os
+import json
 
 from database import db, Todo
+from recommendation_engine import RecommendationEngine
 
 
 app = Flask(__name__)
@@ -50,6 +52,43 @@ def remove_todo(id):
     db.session.delete(Todo.query.filter_by(id=id).first())
     db.session.commit()
     return redirect(url_for("index"))
+
+# recommend a to-do item AI
+@app.route('/recommend/<int:id>', methods=['GET'])
+@app.route('/recommend/<int:id>/<refresh>', methods=['GET'])
+async def recommend(id, refresh=False):
+    recommendation_engine = RecommendationEngine()
+    g.todo = db.session.query(Todo).filter_by(id=id).first()
+
+    if g.todo and not refresh:
+        try:
+            #attempt to load any saved recommendation from the DB
+            if g.todo.recommendations_json is not None:
+                g.todo.recommendations = json.loads(g.todo.recommendations_json)
+                return render_template('index.html')
+        except ValueError as e:
+            print("Error:", e)
+
+    previous_links_str = None
+    if refresh:
+        g.todo.recommendations = json.loads(g.todo.recommendations_json)
+        # Extract links
+        links = [item["link"] for item in g.todo.recommendations]
+        # Convert list of links to a single string
+        previous_links_str = ", ".join(links)
+
+    g.todo.recommendations = await recommendation_engine.get_recommendations(g.todo.name, previous_links_str)
+        
+    # Save the recommendations to the database
+    try:
+        g.todo.recommendations_json = json.dumps(g.todo.recommendations)
+        db.session.add(g.todo)
+        db.session.commit()
+    except Exception as e:
+        print(f"Error adding and committing todo: {e}")
+        return
+
+    return render_template('index.html')
 
 
 
